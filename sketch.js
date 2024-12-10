908a296d9f798a4eda837d8dbbf4a6c852c4f103
Ivy-langley <ivy.mary.langley@gmail.com>
2024-12-10 14:57:17 +0000

    Perlin noise sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
 function setup() {
-  createCanvas(400, 400);
-  fill('red');
-}
-
-function draw() {
-  background(220);
-}
+    createCanvas(600, 600);
+  
+    describe('A calm sea drawn in gray against a black sky.');
+  }
+  
+  function draw() {
+    background('#000000');
+  
+    // Set the noise level and scale.
+    let noiseLevel = 700;
+    let noiseScale = 0.006;
+  
+    // Iterate from left to right.
+    for (let x = 0; x < 600; x += 1) {
+      // Scale the input coordinates.
+      let nx = noiseScale * x;
+      let nt = noiseScale * frameCount;
+  
+      // Compute the noise value.
+      let y = noiseLevel * noise(nx, nt);
+  
+      // Draw the line.
+      line(x, 300 , x, y);
+     stroke('#007fbf');
+  
+     line (300, x, x, y);
+     stroke('#001d54');
+  
+      line(y, 300 , x, y);
+      stroke('#007fbf');
+  
+      line(300, y , x, y);
+      stroke('#001d54');
+    }
+  }
